2cfc9143feba6697fea7a1f33d63e87318cea90a
Sergey Dyakov <admin@marchd.ru>
2016-07-11 06:03:31 +0300

    web ast api & web socket test

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var server = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.use(express.static('static'));
@@ -9,10 +10,60 @@ app.get('/', function(req, res){
   res.sendfile('index.html');
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-});
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
+
+server.listen(3001);
+
+// Create a Socket.IO instance, passing it our server
+var socket = io.listen(server);
+
+// Add a connect listener
+socket.on('connection', function(client){ 
+	console.log('Client has connected');
+	// Success!  Now listen to messages to be received
+	client.on('message',function(event){ 
+		console.log('Received message from client!',event);
+	});
+	client.on('disconnect',function(){
+		//clearInterval(interval);
+		console.log('Server has disconnected');
+	});
+	
+
+});
+
+
+
+
+
+var net = require('net');
+
+var clienttcp = new net.Socket();
+clienttcp.connect(5038, '127.0.0.1', function() {
+	console.log('ATS API Connected');
+	fs = require('fs');
+	fs.readFile('/var/www/pass', 'utf8', function (err,fdata) {
+										if (err) {
+											return console.log(err);
+											}
+											var auth='Action: login\nUsername: admin\nSecret: '+fdata+'\n\n'
+											console.log(auth);
+											clienttcp.write(auth);
+										});
+});
+
+clienttcp.on('data', function(data) {
+		if(/Event: PeerStatus/i.test(data))
+		console.log(''+data);
+	    socket.emit('sip', { Event: ''+data });
+		
+});
+
+clienttcp.on('close', function() {
+	console.log('Connection closed');
+});
+
+
